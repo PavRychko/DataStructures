aa8b46bae87f9a46f8b4729c16d3a8fa91b3e5c6
pavRychko <pav.rychko@gmail.com>
2023-01-22 19:49:55 +0200

    initial commit/first steps:  - create constuctors  - add default initiate size  - implemented size, isEmpty and contains methods

---
+++ src/CustomArrayList.java
@@ -0,0 +1,135 @@
+import java.util.*;
+
+public class CustomArrayList implements List {
+
+    private int size = 0;
+    private Object[] array;
+    private static final int DEFAULT_INITIAL_SIZE = 16;
+
+    public CustomArrayList(){
+        initiateArray(DEFAULT_INITIAL_SIZE);
+    }
+
+    public CustomArrayList(int size){
+        initiateArray(size);
+    }
+
+    private void initiateArray(int size){
+        array = new Object[size];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size > 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return Arrays.stream(array).anyMatch(o1 -> o1.equals(o));
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return array;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
